187378241b17faa01059e808087205b5ec6042bf
Mark Monce <moncemark@gmail.com>
2024-07-02 21:32:00 -0400

    Forgot to add models.py

--- models.py
+++ models.py
@@ -2,10 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    firstnamme = db.Column(db.String(100), nullable=False)
+    lastnamme = db.Column(db.String(100), nullable=False)
+    address1 = db.Column(db.String(100), nullable=False)
+    address2 = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(100), nullable=False)
+    zip = db.Column(db.String(100), nullable=False)
+    hourlyrate = db.Column(db.Float)
+    age = db.Column(db.Int)
 
     def __repr__(self):
         return f'<User {self.name}>'
